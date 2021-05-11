dd1ef60197015f46c8c6214a73a6ebba79643bb1
Dokhyam <dokhyam.hoshen@mail.huji.ac.il>
2021-05-11 11:27:48 +0300

    Update getCLIPscores.py

--- getCLIPscores.py
+++ getCLIPscores.py
@@ -17,9 +17,13 @@ StyleGANGenerator = Generator(1024,512,8)
 StyleGANGenerator.cuda()
 StyleGANGenerator.eval()
 StyleGANGenerator.load_state_dict(ckpt['g_ema'], strict=False)
-for d in directions_list:
+for d_file in directions_list:
+	d = torch.load(directions_path + d_file)
+	alpha =  np.random.uniform(1.0,4.0)
 	input_batch = image_latents[image_ind,:,:]
 	input_cuda = input_batch.cuda().float()
 	out1= StyleGANGenerator([input_cuda.unsqueeze(0)],input_is_latent=True, randomize_noise=True)
 	I1 = out1[0].squeeze(0).transpose(0,1).transpose(1,2).detach().cpu().numpy()
+	out2= StyleGANGenerator([input_cuda.unsqueeze(0) + alpha*d.cuda()],input_is_latent=True, randomize_noise=True)
+	I2 = out2[0].squeeze(0).transpose(0,1).transpose(1,2).detach().cpu().numpy()
 
