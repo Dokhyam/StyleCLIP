3c0b1ea66bc0dc7ba145d1c10970c8259a1d5e75
Dokhyam <dokhyam.hoshen@mail.huji.ac.il>
2021-06-10 12:18:43 +0300

    Update GetCode.py

--- global_directions/GetCode.py
+++ global_directions/GetCode.py
@@ -5,9 +5,9 @@ import os
 import pickle
 import numpy as np
 # from dnnlib import tflib  
-# import tensorflow as tf 
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf 
+# import tensorflow.compat.v1 as tf
+# tf.disable_v2_behavior()
 
 import argparse
 
