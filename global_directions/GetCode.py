3953c6fda14672762897d3ee16c0458dc848c21d
Dokhyam <dokhyam.hoshen@mail.huji.ac.il>
2021-06-29 18:08:17 +0300

    Update GetCode.py

--- global_directions/GetCode.py
+++ global_directions/GetCode.py
@@ -1,96 +1,24 @@
 import os
 import pickle
 import numpy as np
-# from dnnlib import tflib  
+from dnnlib import tflib  
 import tensorflow as tf 
-# import tensorflow.compat.v1 as tf
-# tf.disable_v2_behavior()
 
 import argparse
 
 def LoadModel(dataset_name):
     # Initialize TensorFlow.
-#     init_tf()
-    from stylegan import StyleGAN_G
-    model = StyleGAN_G()
+    tflib.init_tf()
     model_path='./model/'
     model_name=dataset_name+'.pkl'
     
     tmp=os.path.join(model_path,model_name)
     with open(tmp, 'rb') as f:
         _, _, Gs = pickle.load(f)
-    from stylegan import copy_weights_to_keras_model
-    copy_weights_to_keras_model(model.model_mapping, all_weights)
-    copy_weights_to_keras_model(model.model_synthesis, all_weights)
     return Gs
 
 def lerp(a,b,t):
      return a + (b - a) * t
-
-# def init_tf(config_dict: dict = None) -> None:
-#     """Initialize TensorFlow session using good default settings."""
-#     # Skip if already initialized.
-#     if tf.get_default_session() is not None:
-#         return
-
-#     # Setup config dict and random seeds.
-#     cfg = _sanitize_tf_config(config_dict)
-#     np_random_seed = cfg["rnd.np_random_seed"]
-#     if np_random_seed is not None:
-#         np.random.seed(np_random_seed)
-#     tf_random_seed = cfg["rnd.tf_random_seed"]
-#     if tf_random_seed == "auto":
-#         tf_random_seed = np.random.randint(1 << 31)
-#     if tf_random_seed is not None:
-#         tf.random.set_seed(tf_random_seed)
-
-#     # Setup environment variables.
-#     for key, value in list(cfg.items()):
-#         fields = key.split(".")
-#         if fields[0] == "env":
-#             assert len(fields) == 2
-#             os.environ[fields[1]] = str(value)
-
-#     # Create default TensorFlow session.
-#     create_session(cfg, force_as_default=True)
-
-# def create_session(config_dict: dict = None, force_as_default: bool = False):
-#     """Create tf.Session based on config dict."""
-#     # Setup TensorFlow config proto.
-#     cfg = _sanitize_tf_config(config_dict)
-#     config_proto = tf.compat.v1.ConfigProto()
-    
-#     for key, value in cfg.items():
-#         fields = key.split(".")
-#         if fields[0] not in ["rnd", "env"]:
-#             obj = config_proto
-#             for field in fields[:-1]:
-#                 obj = getattr(obj, field)
-#             setattr(obj, fields[-1], value)
-
-#     # Create session.
-#     session = tf.Session(config=config_proto)
-#     if force_as_default:
-#         # pylint: disable=protected-access
-#         session._default_session = session.as_default()
-#         session._default_session.enforce_nesting = False
-#         session._default_session.__enter__() # pylint: disable=no-member
-
-#     return session
-
-
-# def _sanitize_tf_config(config_dict: dict = None) -> dict:
-#     # Defaults.
-#     cfg = dict()
-#     cfg["rnd.np_random_seed"]               = None      # Random seed for NumPy. None = keep as is.
-#     cfg["rnd.tf_random_seed"]               = "auto"    # Random seed for TensorFlow. 'auto' = derive from NumPy random state. None = keep as is.
-#     cfg["env.TF_CPP_MIN_LOG_LEVEL"]         = "1"       # 0 = Print all available debug info from TensorFlow. 1 = Print warnings and errors, but disable debug info.
-#     cfg["graph_options.place_pruned_graph"] = True      # False = Check that all ops are available on the designated device. True = Skip the check for ops that are not used.
-#     cfg["gpu_options.allow_growth"]         = True      # False = Allocate all GPU memory at the beginning. True = Allocate only as much GPU memory as needed.
-#     # User overrides.
-#     if config_dict is not None:
-#         cfg.update(config_dict)
-#     return cfg
 
 #stylegan-ada
 def SelectName(layer_name,suffix):
@@ -261,8 +189,8 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
     random_state=5
-    num_img=100000 
-    num_once=1000
+    num_img=100_000 
+    num_once=1_000
     dataset_name=args.dataset_name
     
     if not os.path.isfile('./model/'+dataset_name+'.pkl'):
@@ -280,7 +208,7 @@ if __name__ == "__main__":
 #        GetImg(Gs,num_img=num_img,num_once=num_once,dataset_name=dataset_name,save_name='images_100K') #no need 
     elif args.code_type=='s':
         save_name='S'
-        save_tmp=GetS(dataset_name,num_img=2000)
+        save_tmp=GetS(dataset_name,num_img=2_000)
         tmp='./npy/'+dataset_name+'/'+save_name
         with open(tmp, "wb") as fp:
             pickle.dump(save_tmp, fp)
